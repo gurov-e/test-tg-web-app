5e82dd1469414d36ca12534f4fb5de1e11e1a96e
Гуров Евгений Александрович <gurovea@srvhub.ru>
2024-09-23 14:32:29 +0300

    изменение главной отображаемой страницы

---
+++ src/pages/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+const App = () => {
+    return (
+        <div className={styles.app}>App</div>
+    );
+};
+
+export default App;
