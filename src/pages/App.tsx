bb84342bf1b8a0c3400ba08c225b87aea1f1d78c
Гуров Евгений Александрович <gurovea@srvhub.ru>
2024-09-23 15:10:34 +0300

    попытка 1 получить initData

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,8 @@
 import styles from './App.module.scss';
+import WebApp from '@twa-dev/sdk';
 
 const App = () => {
+    alert(WebApp.initData);
     return (
         <div className={styles.app}>App</div>
     );
